c7268400dd312a424dbc83d671fd04fc7d7065ab
sridivyaguttula20 <sridivyaguttula20@gmail.com>
2026-05-16 18:16:45 +0530

    keep only basics package

---
+++ src/basics/A_HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+Topic: Basics
+Program: Hello World
+
+Definition:
+This program prints "Hello World" on the screen.
+
+Approach:
+Using System.out.println() method.
+
+Note:
+Some tutorials use shortcut syntax like:
+void main() {
+   System.out.println("Hello World");
+}
+*/
+
+package basics;
+public class A_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/basics/H_EscapeSequences.java
@@ -0,0 +1,34 @@
+/*
+Topic: Basics
+Program: Escape Sequences in Java
+
+Definition:
+Escape sequences are special characters used inside strings.
+
+Common Escape Sequences:
+1. \n  -> New Line
+2. \t  -> Tab Space
+3. \"  -> Double Quotes
+4. \\  -> Backslash
+
+Approach:
+Using different escape sequences inside print statements.
+*/
+
+package basics;
+public class H_EscapeSequences {
+    public static void main(String[] args) {
+
+        // New Line
+        System.out.println("Hello\nWorld");
+
+        // Tab Space
+        System.out.println("Java\tProgramming");
+
+        // Double Quotes
+        System.out.println("\"Welcome to Java\"");
+
+        // Backslash
+        System.out.println("C:\\Users\\Divya");
+    }
+}
